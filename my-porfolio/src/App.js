1ad6bec5ba7888bfee9f28b6150144edec5d6a03
r8s11 <r8s11@github.com>
2019-11-07 17:03:00 -0500

    wip

--- my-porfolio/src/App.js
+++ my-porfolio/src/App.js
@@ -1,25 +1,165 @@
 import React from 'react';
-import logo from './logo.svg';
+ 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  <div className="App">
+    <body>
+      <div>
+        <h1>Roosevelt B. Segura</h1>
+        <h2>Roosevelt.bseg@gmail.com</h2>
+      </div>
+      <div>
+        <h4>Summary</h4>
+        <p>Experience worker in IT while continuing my education in the field of Cyber Security. Strong customer service over the phone assisting with installing, modifying and repairing computer hardware and software.</p>
+      </div>
+      <div>
+        <h4>Skills & Abilities</h4>
+          <ul>
+            Microsoft Windows OS,
+            Microsoft Office Applications
+            Mac OS 
+            Network Security knowledge
+            Bilingual (English and Spanish)
+            Superior Customer Service
+          </ul>
+      </div>
+      <div>
+        <h4>Certifications</h4>
+          <p>
+            CompTIA Security+ CE Apr/2016 – Apr/2019
+          </p>
+      </div>
+      <div>
+        <h4>
+          Training
+        </h4>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          •	Windows Server 2012 R2 (40 Hours)
+          •	Windows 7 (40 hours)
+          •	Linux
+          •	Network Plus (40 hours)
+          •	Security Plus (40 hours)
+          •	AWS
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+      <div className="Experience jobs">
+        <h4>Experience</h4>
+        <div>
+          <p>
+            Partners HealthCare Boston, MA			May/2019 – Present
+            Imaging Informatics Analyst
+          </p>
+          <ul>
+            •	Collaborate and communicate effectively with colleagues, and other hospital personnel, to facilitate day to day operations.
+            •	Troubleshoot / repair hardware and software problems.
+            •	Install patches / upgrades to Desktop / Office based applications.
+            •	Coordinates with System Administrators on mission critical application issues.
+            •	Provide training and documentation instructions for hardware and software operation for clinical systems.
+            •	Provide phone, e-mail, on-site, and off-site direct user support as required.
+            •	Investigates, plans, implements, tests, and debugs operating system software.
+            •	Troubleshoots and resolves hardware, software, and connectivity problems, including user access and component configuration.
+            •	Provides advanced technical assistance and maintenance support to departmental end
+            users.
+            •	Records and maintains hardware and software inventories for ender user license
+            agreements.
+            •	Performs problem resolution and provides on-call support for computer/network systems.
+          </ul>
+        </div>
+        <div>
+          <p>
+            MIT Lincoln Laboratory Lexington, MA			Apr/2019 – May/2019
+            System Support Analyst
+          </p>
+          <ul>
+            •	Provide desktop and laptop desk side support for Windows & Mac systems
+            •	Provide technical support and training to personal computer and PC network users
+            •	Assist users in solving IT problems using available hardware and software tools
+            •	Support, install, and maintain personal computers, local area networks, operating systems, software, and related IT peripherals
+            •	Install and support hardware and software components for user groups, perform preventive maintenance, and test equipment
+            •	Evaluate system configuration and software to ensure effective use of hardware resources
+            •	Engage with users to determine their potential future business requirements and create ways to streamline making these requirements a reality
+            •	Provide positive customer experiences when working with users
+            •	Use “Footprints” ticketing system daily to triage tickets, escalations, issues, etc.
+          </ul>
+        </div>
+        <div>
+          <p>
+            Netscout Westford, MA					Nov/2018 – Mar/2019
+            IT Helpdesk Support Specialist
+          </p>
+          <ul>
+            •	Provide desktop end-user support, PC setup/configuration and resolution of all IT Client Services requests, for local and remote employees.
+            •	Provide technical support to end users in support of hardware, software, peripherals, and network infrastructure.
+            •	Build and deploy various brands of personal computers with associated configurations.
+            •	Deploy and support VOIP phone hardware and software.
+            •	Maintain and enforce corporate hardware, software, and anti-virus policies.
+            •	Update online helpdesk ticketing system, to track progress and status.
+            •	Attend regular staff meetings to report on tasks.
+            •	Act as a resource for all employees regarding technical questions, and training.
+          </ul>
+        </div>
+     
+        
+        <div>
+          <p>
+            MilliporeSigma Danvers, MA				Apr/2018 – Oct/2018 
+            Assembly
+          </p>
+          
+          <ul>
+            •	Reading detailed instructions and following established procedures.
+            •	producing and packaging medical disposables in a clean-room, biopharmaceutical environment.
+            •	Maintains safe and clean working environment by complying with procedures, rules, and regulations.
+          </ul>
+        </div>
+        
+        <div>
+          <p>
+            OnePath North Andover, MA                                          	Aug/2017 – Jan/2018
+            Help Desk Technician I
+          </p>
+          <ul>
+            •	Install, modify, and repair computer hardware and software.
+            •	Manage Active Directory.
+            •	Manage and Troubleshoot Office 365 email.
+            •	Troubleshoot MS Windows 7 & 10.
+            •	Coordinate with remote users for PC repairs and diagnostics.
+            •	Troubleshoot network connectivity issues, working with remote users on a corporate network.
+          </ul>        
+        </div>
+        <p>
+          G4S Secure Integration Burlington, MA                         Oct/2016 – March/2017
+          Help Desk Technician I
+        </p>
+
+        <ul>
+          •	Monitor hardware, software and networks.
+          •	Ability to use ticketing systems to track, manage, review machine issues.
+        </ul>
+      
+      </div>
+      <div>
+        <p>
+          <h4>Military</h4>
+        
+          <p>
+            U.S. Army Specialist (Reserves)		          Mar/2010 - Mar/2018
+            21W Carpentry Specialist
+          </p>
+        </p>
+      </div>
+      <div>
+        <p>
+          <h4>Education</h4>
+          
+          Bunker hill Community College                                         	2019/2020
+          •	Field of Study: Cybersecurity
+        </p>
+      </div>
+    </body>
+  </div>
   );
 }
 
