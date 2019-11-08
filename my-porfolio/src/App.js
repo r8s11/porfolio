95e44f0ddb6af09281149431f56c8899510e31e2
r8s11 <r8s11@github.com>
2019-11-08 16:13:20 -0500

    HTML and little CSS

--- my-porfolio/src/App.js
+++ my-porfolio/src/App.js
@@ -11,152 +11,146 @@ function App() {
         <h2>Roosevelt.bseg@gmail.com</h2>
       </div>
       <div>
-        <h4>Summary</h4>
+        <h3>Summary</h3>
         <p>Experience worker in IT while continuing my education in the field of Cyber Security. Strong customer service over the phone assisting with installing, modifying and repairing computer hardware and software.</p>
       </div>
       <div>
-        <h4>Skills & Abilities</h4>
+        <h3>Skills & Abilities</h3>
           <ul>
-            Microsoft Windows OS,
-            Microsoft Office Applications
-            Mac OS 
-            Network Security knowledge
-            Bilingual (English and Spanish)
-            Superior Customer Service
+            <li>Microsoft Windows OS</li>
+            <li>Microsoft Office Applications</li>
+            <li>Mac OS</li> 
+            <li>Network Security knowledge</li>
+            <li>Bilingual (English and Spanish)</li>
+            <li>Superior Customer Service</li>
           </ul>
       </div>
       <div>
-        <h4>Certifications</h4>
-          <p>
-            CompTIA Security+ CE Apr/2016 – Apr/2019
-          </p>
+        <h3>Certifications</h3>
+          <ul>
+            <p>CompTIA Security+ CE Apr/2016 – Apr/2019</p>
+          </ul>
       </div>
       <div>
-        <h4>
+        <h3>
           Training
-        </h4>
-        <p>
-          •	Windows Server 2012 R2 (40 Hours)
-          •	Windows 7 (40 hours)
-          •	Linux
-          •	Network Plus (40 hours)
-          •	Security Plus (40 hours)
-          •	AWS
-        </p>
+        </h3>
+        <ul>
+        <li>Windows Server 2012 R2 (40 Hours)</li>
+        <li> Windows OS (40 hours)</li>
+        <li>Network+ (40 hours)</li>
+        <li>Security+ (40 hours)</li>
+        <li>Linux</li>
+        </ul>
       </div>
       <div className="Experience jobs">
-        <h4>Experience</h4>
+        <h3>Experience</h3>
         <div>
-          <p>
+          <h4>
             Partners HealthCare Boston, MA			May/2019 – Present
             Imaging Informatics Analyst
-          </p>
+          </h4>
           <ul>
-            •	Collaborate and communicate effectively with colleagues, and other hospital personnel, to facilitate day to day operations.
-            •	Troubleshoot / repair hardware and software problems.
-            •	Install patches / upgrades to Desktop / Office based applications.
-            •	Coordinates with System Administrators on mission critical application issues.
-            •	Provide training and documentation instructions for hardware and software operation for clinical systems.
-            •	Provide phone, e-mail, on-site, and off-site direct user support as required.
-            •	Investigates, plans, implements, tests, and debugs operating system software.
-            •	Troubleshoots and resolves hardware, software, and connectivity problems, including user access and component configuration.
-            •	Provides advanced technical assistance and maintenance support to departmental end
-            users.
-            •	Records and maintains hardware and software inventories for ender user license
-            agreements.
-            •	Performs problem resolution and provides on-call support for computer/network systems.
+            <li>Collaborate and communicate effectively with colleagues, and other hospital personnel, to facilitate day to day operations.</li>
+            <li>Troubleshoot / repair hardware and software problems.</li>
+            <li>Install patches / upgrades to Desktop / Office based applications.</li>
+            <li>Coordinates with System Administrators on mission critical application issues.</li>
+            <li>Provide training and documentation instructions for hardware and software operation for clinical systems.</li>
+            <li>Provide phone, e-mail, on-site, and off-site direct user support as required.</li>
+            <li>Investigates, plans, implements, tests, and debugs operating system software.</li>
+            <li>Troubleshoots and resolves hardware, software, and connectivity problems, including user access and component configuration.</li>
+            <li>Provide advanced technical assistance and maintenance support to department and users.</li>
+            <li>Records and maintains hardware and software inventories for ender user license agreements.</li>
+            <li>Performs problem resolution and provides on-call support for computer/network systems.</li>
           </ul>
         </div>
         <div>
-          <p>
-            MIT Lincoln Laboratory Lexington, MA			Apr/2019 – May/2019
-            System Support Analyst
-          </p>
+          <h4>
+              MIT Lincoln Laboratory Lexington, MA			Apr/2019 – May/2019
+              System Support Analyst            
+          </h4>
           <ul>
-            •	Provide desktop and laptop desk side support for Windows & Mac systems
-            •	Provide technical support and training to personal computer and PC network users
-            •	Assist users in solving IT problems using available hardware and software tools
-            •	Support, install, and maintain personal computers, local area networks, operating systems, software, and related IT peripherals
-            •	Install and support hardware and software components for user groups, perform preventive maintenance, and test equipment
-            •	Evaluate system configuration and software to ensure effective use of hardware resources
-            •	Engage with users to determine their potential future business requirements and create ways to streamline making these requirements a reality
-            •	Provide positive customer experiences when working with users
-            •	Use “Footprints” ticketing system daily to triage tickets, escalations, issues, etc.
+            <li>Provide desktop and laptop desk side support for Windows & Mac systems.</li>
+            <li>Provide technical support and training to personal computer and PC network users.</li>
+            <li>Assist users in solving IT problems using available hardware and software tools.</li>
+            <li>Support, install, and maintain personal computers, local area networks, operating systems, software, and related IT peripherals.</li>
+            <li>Install and support hardware and software components for user groups, perform preventive maintenance, and test equipment.</li>
+            <li>Evaluate system configuration and software to ensure effective use of hardware resources.</li>
+            <li>Engage with users to determine their potential future business requirements and create ways to streamline making these requirements a reality.</li>
+            <li>Provide positive customer experiences when working with users.</li>
+            <li>Use “Footprints” ticketing system daily to triage tickets, escalations, issues, etc.</li>
           </ul>
         </div>
         <div>
-          <p>
+          <h4>
             Netscout Westford, MA					Nov/2018 – Mar/2019
             IT Helpdesk Support Specialist
-          </p>
+          </h4>
           <ul>
-            •	Provide desktop end-user support, PC setup/configuration and resolution of all IT Client Services requests, for local and remote employees.
-            •	Provide technical support to end users in support of hardware, software, peripherals, and network infrastructure.
-            •	Build and deploy various brands of personal computers with associated configurations.
-            •	Deploy and support VOIP phone hardware and software.
-            •	Maintain and enforce corporate hardware, software, and anti-virus policies.
-            •	Update online helpdesk ticketing system, to track progress and status.
-            •	Attend regular staff meetings to report on tasks.
-            •	Act as a resource for all employees regarding technical questions, and training.
+            <li>Provide desktop end-user support, PC setup/configuration and resolution of all IT Client Services requests, for local and remote employees.</li>
+            <li>Provide technical support to end users in support of hardware, software, peripherals, and network infrastructure.</li>
+            <li>Build and deploy various brands of personal computers with associated configurations.</li>
+            <li>Deploy and support VOIP phone hardware and software.</li>
+            <li>Maintain and enforce corporate hardware, software, and anti-virus policies.</li>
+            <li>Update online helpdesk ticketing system, to track progress and status.</li>
+            <li>Attend regular staff meetings to report on tasks.</li>
+            <li>Act as a resource for all employees regarding technical questions, and training.</li>
           </ul>
         </div>
-     
-        
         <div>
-          <p>
+          <h4>
             MilliporeSigma Danvers, MA				Apr/2018 – Oct/2018 
             Assembly
-          </p>
+          </h4>
           
           <ul>
-            •	Reading detailed instructions and following established procedures.
-            •	producing and packaging medical disposables in a clean-room, biopharmaceutical environment.
-            •	Maintains safe and clean working environment by complying with procedures, rules, and regulations.
+            <li>Reading detailed instructions and following established procedures.</li>
+            <li>producing and packaging medical disposables in a clean-room, biopharmaceutical environment.</li>
+            <li>Maintains safe and clean working environment by complying with procedures, rules, and regulations.</li>
           </ul>
         </div>
         
         <div>
-          <p>
+          <h4>
             OnePath North Andover, MA                                          	Aug/2017 – Jan/2018
             Help Desk Technician I
-          </p>
+          </h4>
           <ul>
-            •	Install, modify, and repair computer hardware and software.
-            •	Manage Active Directory.
-            •	Manage and Troubleshoot Office 365 email.
-            •	Troubleshoot MS Windows 7 & 10.
-            •	Coordinate with remote users for PC repairs and diagnostics.
-            •	Troubleshoot network connectivity issues, working with remote users on a corporate network.
+            <li>Install, modify, and repair computer hardware and software.</li>
+            <li>Manage Active Directory.</li>
+            <li>Manage and Troubleshoot Office 365 email.</li>
+            <li>Troubleshoot MS Windows 7 & 10.</li>
+            <li>Coordinate with remote users for PC repairs and diagnostics.</li>
+            <li>Troubleshoot network connectivity issues, working with remote users on a corporate network.</li>
           </ul>        
         </div>
-        <p>
+        <div>
+        <h4>
           G4S Secure Integration Burlington, MA                         Oct/2016 – March/2017
           Help Desk Technician I
-        </p>
-
+        </h4>
         <ul>
-          •	Monitor hardware, software and networks.
-          •	Ability to use ticketing systems to track, manage, review machine issues.
+          <li>Monitor hardware, software and networks.</li>
+          <li>Ability to use ticketing systems to track, manage, review machine issues.</li>
         </ul>
+        </div>
       
       </div>
       <div>
-        <p>
-          <h4>Military</h4>
+          <h3>Military</h3>
         
-          <p>
+          <ul>
             U.S. Army Specialist (Reserves)		          Mar/2010 - Mar/2018
             21W Carpentry Specialist
-          </p>
-        </p>
+          </ul>
       </div>
       <div>
-        <p>
-          <h4>Education</h4>
+          <h3>Education</h3>
           
-          Bunker hill Community College                                         	2019/2020
-          •	Field of Study: Cybersecurity
-        </p>
+          <ul>
+            Bunker hill Community College -                                        
+            Field of Study: Cybersecurity
+          </ul>
       </div>
     </body>
   </div>
